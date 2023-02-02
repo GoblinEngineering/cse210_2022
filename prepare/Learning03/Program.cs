03e9a92979d15bc517210977005cdeabca924ea7
GoblinEngineering <bacretrac@gmail.com>
2023-02-01 20:23:20 -0600

    week5_midweek

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac = new Fraction(1,2);
+        int top_num;
+        int bot_num;
+        void tester(int top, int bot)
+        {
+            frac.SetTop(top);
+            frac.SetBottom(bot);
+            Console.WriteLine(frac.GetTop());
+            Console.WriteLine(frac.GetBottom());
+            Console.WriteLine(frac.GetFractionString());
+            Console.WriteLine(frac.GetDecimalValue());
+            
+        }
+        tester(1,1);
+        tester(5,1);
+        tester(3,4);
+        tester(1,3);
+        Console.Write("top num : ");
+        top_num = int.Parse(Console.ReadLine());
+        Console.Write("bot num : ");
+        bot_num = int.Parse(Console.ReadLine());
+        tester(top_num,bot_num);
     }
 }
