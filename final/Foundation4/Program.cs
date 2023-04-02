83509945033badf5f725c22b859dc30eebccd0ce
GoblinEngineering <bacretrac@gmail.com>
2023-04-01 23:18:37 -0500

    foundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Running day1 = new Running("10/2/2020", 60, 2);
+        Bicycle day2 = new Bicycle("10/3/2020", 120, 3);
+        Swim day3 = new Swim("10/4/2020", 30, 5);
+
+        List<Activity> list = new List<Activity>();
+        list.Add(day1);
+        list.Add(day2);
+        list.Add(day3);
+
+        foreach (Activity day in list)
+        {
+            //
+            day.display();
+        }
+
     }
 }
